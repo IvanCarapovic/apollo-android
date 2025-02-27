bec3ae6c6323e142d308f614787f2cd4d5edd50f
Ivan Carapovic <developer@chapz.dev>
2025-02-27 19:52:08 +0100

    Add initial files

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "dev.chapz.apollo"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "dev.chapz.apollo"
+        minSdk = 31
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.activity)
+    implementation(libs.compose.graphics)
+    implementation(libs.compose.ui.m3)
+    implementation(libs.compose.tooling)
+    implementation(libs.compose.tooling.preview)
+    implementation(libs.compose.junit)
+    implementation(libs.compose.navigation)
+    implementation(libs.compose.runtime.livedata)
+
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.core)
+
+    implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+
+    testImplementation(libs.junit)
+}

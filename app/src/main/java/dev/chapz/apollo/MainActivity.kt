cc011693a0b8e949c6faf10f628d2d18bcd28835
Ivan Carapovic <20989903+icarapovic@users.noreply.github.com>
2025-05-26 14:20:11 +0200

    Instantiate the MainViewModel and start the media service

--- app/src/main/java/dev/chapz/apollo/MainActivity.kt
+++ app/src/main/java/dev/chapz/apollo/MainActivity.kt
@@ -1,9 +1,11 @@
 package dev.chapz.apollo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
@@ -11,12 +13,20 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import dev.chapz.apollo.navigation.BottomNavigationBar
 import dev.chapz.apollo.navigation.NavigationWrapper
+import dev.chapz.apollo.player.MediaPlayerService
 import dev.chapz.apollo.ui.theme.ApolloTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // setup media service and controller
+        startService(Intent(this, MediaPlayerService::class.java))
+        viewModel.initMediaController()
+
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
